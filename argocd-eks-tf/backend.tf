7f609f5ec5432ad3c68ca4bef3d8a0609528fc38
amran <alimran91@hotmail.com>
2026-05-01 18:27:30 +0600

    Add backend with required providers tf code

---
+++ argocd-eks-tf/backend.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "s3" {
+    bucket       = "dev-aman-tf-bucket"
+    region       = "us-east-1"
+    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    use_lockfile = true
+    encrypt      = true
+  }
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+  }
+}
